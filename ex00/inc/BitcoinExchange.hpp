78ade571c828be15155a23fc083db446c42737da
Niklas Weber <nweber@student.42Heilbronn.de>
2025-11-23 19:28:01 +0100

    Add BitcoinExchange class definition with member functions and data members

--- ex00/inc/BitcoinExchange.hpp
+++ ex00/inc/BitcoinExchange.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <iostream>
+#include <map>
+#include <exception>
+class BitcoinExchange {
+  public:
+    BitcoinExchange();
+    ~BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange& other);
+    BitcoinExchange& operator=(const BitcoinExchange& other);
+
+    void loadData(const std::string& filename);
+    void processInput(const std::string& filename) const;
+  private:
+    std::map<std::string, double> data;
+    bool isValidDate(const std::string& date) const;
+    double getExchangeRate(const std::string& date) const;
+    void parseLine(const std::string& line, std::string& date, double& value) const;
+};
